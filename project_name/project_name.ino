06216fd7baa6e8885c9b9a5134c556dc5bd27c48
Mrgom1 <nicolasbreil2@gmail.com>
2025-12-03 10:50:32 +0100

    Init du projet

--- project_name/project_name.ino
+++ project_name/project_name.ino
@@ -6,10 +6,15 @@ void setup()
 {
   // put your setup code here, to run once:
   myApplication.init();
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() 
 {
   // put your main code here, to run repeatedly:
   myApplication.run();
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the  LED on by making the pin 13 HIGH
+  delay(500);              // wait for a 0.5  second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the  pin 13 LOW
+  delay(500);              // wait for a 0.5 second
 }
